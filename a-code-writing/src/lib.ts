88687fef98de73c994a0657a9d106360aa0d4527
Jordan O'Leary <jdoleary@gmail.com>
2021-01-16 15:53:18 -0500

    chore: format
    src: Add interfaces

--- a-code-writing/src/lib.ts
+++ a-code-writing/src/lib.ts
@@ -1,31 +1,41 @@
-
 import axios from 'axios';
 import { encode } from 'querystring'
 
 import { get_query_area_config, get_query_count_config, api_url } from './config'
 
+interface FeatureRecord {
+    attributes: {
+        'Shape.STArea()': number;
+    }
+}
+
 export default async function main() {
-    let all_features = [];
-    let shouldQueryNextPage = false;
-    const query_area_inst = get_query_area_config();
-    const query_count_inst = get_query_count_config();
     let feature_count = 0;
 
-    try{
+    try {
         console.log('Querying parcel counts...');
-        const { data } = await axios.get(`${api_url}?${encode(query_count_inst)}`);
+        const { data }: { data: { count: number } } = await axios.get(`${api_url}?${encode(get_query_count_config())}`);
         feature_count = data.count;
-        console.log(`There are ${feature_count} features.`);
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
 
+    let all_features: FeatureRecord[] = [];
+    let shouldQueryNextPage = false;
+    const query_area_config = get_query_area_config();
     try {
         console.log('Querying parcel data...');
         do {
-            const { data } = await axios.get(`${api_url}?${encode(query_area_inst)}`);
+            const { data }: {
+                data: {
+                    features: FeatureRecord[],
+                    exceededTransferLimit: boolean
+                }
+            } = await axios.get(`${api_url}?${encode(query_area_config)}`);
+
             const { features = [], exceededTransferLimit } = data;
-            query_area_inst.resultOffset += features.length;
+            // Setup resultOffset for querying the next "page" of records
+            query_area_config.resultOffset += features.length;
 
             // docs: https://developers.arcgis.com/rest/services-reference/query-feature-service-layer-.htm
             // exceededTransferLimit "indicates there are more query results and you can continue to page through the results"
@@ -33,13 +43,13 @@ export default async function main() {
 
             // Add the features retrieved from this request to the total list of features
             all_features = all_features.concat(features);
-            console.log(`Loaded... ${all_features.length}`);
+            console.log(`Loaded... ${all_features.length} of ${feature_count}`);
         } while (shouldQueryNextPage);
     } catch (e) {
         console.error(e)
     }
 
-    if(all_features.length !== feature_count){
+    if (all_features.length !== feature_count) {
         console.error('Paginated query did not return all features.');
     }
 
@@ -47,5 +57,6 @@ export default async function main() {
         sum += current.attributes['Shape.STArea()'];
         return sum;
     }, 0);
-    console.log(`The average parcel size is ${sum_of_areas/feature_count}`);
+
+    console.log(`Of ${feature_count} parcels, the average area is ${sum_of_areas / feature_count}`);
 }
