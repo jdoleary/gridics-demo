17fa81c6fb5bb8302d90011610fc2b5e65a8a43b
Jordan O'Leary <jdoleary@gmail.com>
2021-01-16 16:59:30 -0500

    src: Differentiate Cell formula from Cell number
    It is more clear to store objects such as SumFormula in their own
    property.
    
    Override valueOf() so that the Cell will return the value of it's
    contents when it is cast as a primitive.

--- b-component-design/src/Cell.js
+++ b-component-design/src/Cell.js
@@ -1,17 +1,26 @@
 class Cell {
-    constructor(contents){
-        this.contents = contents;
+  constructor(contents) {
+    this.change(contents);
+  }
+  change(contents) {
+    if (typeof contents == "object") {
+      this.formula = contents;
+    } else if (typeof contents == "number") {
+      this.number = contents;
+    } else {
+      throw new Error(`${contents} is not a valid value for Cell`);
     }
-    change(newValue){
-        this.contents = newValue;
+  }
+  valueOf() {
+    if (this.formula) {
+      return this.formula.calculate();
+    } else {
+      return this.number;
     }
-    toString(){
-        if(typeof this.contents === 'object'){
-            return this.contents.calculate();
-        }
-        return this.contents;
-
-    }
+  }
+  toString() {
+    return this.valueOf();
+  }
 }
 
 module.exports = Cell;
