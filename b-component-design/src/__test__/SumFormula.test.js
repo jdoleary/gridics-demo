2ccc89adfb2355d74e41bbb00341182549e77e8a
Jordan O'Leary <jdoleary@gmail.com>
2021-01-16 16:52:57 -0500

    test: Test for nested formulas in Cells

--- b-component-design/src/__test__/SumFormula.test.js
+++ b-component-design/src/__test__/SumFormula.test.js
@@ -15,4 +15,15 @@ describe("SumFormula", () => {
     expected = 8;
     expect(actual).toEqual(expected);
   });
+  it("should support nested formulas", () => {
+    const a1 = new Cell(7);
+    const a2 = new Cell(3);
+    const a3 = new Cell(5);
+    const a4 = new Cell(new SumFormula([a1, a2, a3]));
+    const a5 = new Cell(10);
+    const a6 = new Cell(new SumFormula([a4, a5]));
+    let actual = a6.toString();
+    let expected = 7 + 3 + 5 + 10;
+    expect(actual).toEqual(expected);
+  });
 });
