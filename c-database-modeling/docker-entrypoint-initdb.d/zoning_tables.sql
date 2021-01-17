4eab7f4a2a608d68198e0727cd6f8756e3c3108b
Jordan O'Leary <jdoleary@gmail.com>
2021-01-17 09:54:27 -0500

    sql: Format sql for readability

--- c-database-modeling/docker-entrypoint-initdb.d/zoning_tables.sql
+++ c-database-modeling/docker-entrypoint-initdb.d/zoning_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS rule (
     id serial primary key,
     levels_max integer,
-    lot_coverage_max real,
+    lot_coverage_max numeric(3, 2),
     setback_front integer,
     setback_side integer,
     setback_rear integer,
@@ -28,68 +28,140 @@ CREATE TABLE IF NOT EXISTS parcel (
     lot_width integer,
     lot_length integer,
     zone_id integer,
-    CONSTRAINT fk_zone 
+    CONSTRAINT fk_zone
         FOREIGN KEY(zone_id)
         REFERENCES zone(id)
         ON DELETE SET NULL
 );
 
+INSERT INTO
+    rule (
+        levels_max,
+        lot_coverage_max,
+        setback_front,
+        setback_side,
+        setback_rear,
+        FAR,
+        density,
+        unit_size_min,
+        residential_parking_min,
+        office_parking_min,
+        commercial_parking_min
+    )
+VALUES
+    (
+        2,
+        0.60,
+        10,
+        5,
+        8,
+        0.75,
+        48,
+        1200.00,
+        1.5,
+        0.0020,
+        0.0030
+    );
 
-INSERT INTO rule  (levels_max, lot_coverage_max, setback_front,
-setback_side, setback_rear, FAR, density, unit_size_min, residential_parking_min, office_parking_min, commercial_parking_min)
-VALUES (2, 60.00, 10,5,8,0.75, 48, 1200.00, 1.5, 0.0020, 0.0030);
+INSERT INTO
+    zone (rule_id)
+VALUES
+    (1);
 
-INSERT INTO zone (rule_id)
-VALUES (1);
-
-INSERT INTO parcel  (lot_area, lot_width, lot_length, zone_id)
-VALUES (5000, 50, 100, 1);
-
+INSERT INTO
+    parcel (lot_area, lot_width, lot_length, zone_id)
+VALUES
+    (5000, 50, 100, 1);
 
 CREATE OR REPLACE FUNCTION footprintMax ()
 RETURNS double precision as $$
-BEGIN
-RETURN (select min(
-    -- Footprint by Coverage
-    (lot_area*lot_coverage_max),
-    -- Footprint by Setback
-    ((lot_length-setback_front-setback_rear)*(lot_width-setback_side-setback_side))::double precision
-    ) as footprint_max
-    from zone
-left join rule on zone.rule_id = rule.id
-left join parcel on zone.id = parcel.zone_id);
+BEGIN RETURN (
+    select
+        LEAST(
+            -- Footprint by Coverage
+            (lot_area * lot_coverage_max),
+            -- Footprint by Setback
+            (
+                (lot_length - setback_front - setback_rear) *(lot_width - setback_side - setback_side)
+            )
+        ) as footprint_max
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
+END; $$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION capacityMaxByFAR ()
+RETURNS double precision as $$
+BEGIN RETURN (
+    select
+        lot_area * FAR as capacity_max_by_far
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
+END; $$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION capacityMaxByBulk ()
+RETURNS double precision as $$
+BEGIN RETURN (
+    select
+        footprintMax() * levels_max as capacity_max_by_bulk
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
 END; $$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION capacityMax ()
 RETURNS double precision as $$
-BEGIN
-RETURN (select min(
-    -- Capacity by FAR
-    (lot_area*FAR),
-    -- Capacity by Bulk
-    (footprintMax()*levels_max)
-    ) as capacity_max
-    from zone
-left join rule on zone.rule_id = rule.id
-left join parcel on zone.id = parcel.zone_id);
+BEGIN RETURN (
+    select
+        LEAST(
+            capacityMaxByFAR(),
+            capacityMaxByBulk()
+        ) as capacity_max
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
 END; $$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION dwellingUnitsByDensity ()
 RETURNS double precision as $$
-BEGIN
-RETURN (select (lot_area/43560 * density) as dwelling_units_by_density
-    from zone
-left join rule on zone.rule_id = rule.id
-left join parcel on zone.id = parcel.zone_id);
+BEGIN RETURN (
+    select
+        (lot_area / 43560.0 * density) as dwelling_units_by_density
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
 END; $$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION dwellingUnitsByBulk ()
 RETURNS double precision as $$
-BEGIN
-RETURN (select (capacityMax()/unit_size_min) as dwelling_units_by_bulk
-    from zone
-left join rule on zone.rule_id = rule.id
-left join parcel on zone.id = parcel.zone_id);
+BEGIN RETURN (
+    select
+        (capacityMax() / unit_size_min) as dwelling_units_by_bulk
+    from
+        zone
+        left join rule on zone.rule_id = rule.id
+        left join parcel on zone.id = parcel.zone_id
+);
 END; $$ LANGUAGE plpgsql IMMUTABLE;
 
-select min(ceil(dwellingUnitsByDensity()), ceil(dwellingUnitsByBulk())) as dwelling_units_maximum;
+CREATE OR REPLACE FUNCTION dwellingUnitsMax ()
+RETURNS double precision as $$
+BEGIN RETURN (
+    select
+        LEAST(
+            ceil(dwellingUnitsByDensity()),
+            ceil(dwellingUnitsByBulk())
+        ) as dwelling_units_maximum
+);
+END; $$ LANGUAGE plpgsql IMMUTABLE;
